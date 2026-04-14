a92a7696570155c17a777add4e42617e861f82cb
RyanRafaelA <ryrafael461@gmail.com>
2026-04-14 15:58:05 -0300

    Gerador de password

--- src/services/password/create.js
+++ src/services/password/create.js
@@ -1,7 +1,11 @@
 import chalk from "chalk";
 
+import handle from "../qr-code/handle";
+
 async function createPassword(){
     console.log(chalk.green("password"));
+    const password = await handle;
+    console.log(password);
 }
 
 export default createPassword;
