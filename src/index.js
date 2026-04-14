4e37295d0f6cf2980ff118107c5a103973fa7d46
RyanRafaelA <ryrafael461@gmail.com>
2026-04-14 14:40:45 -0300

    Adicionando o Main Prompt ao index

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log("Hello World!");
+import prompt from 'prompt';
+
+import mainPrompt from "./prompts/prompts-main.js";
+
+async function main(){
+    prompt.get(mainPrompt, async (err, choose) => {
+        if(choose.select == 1) console.log("Escolheu o qrCODE");
+        if(choose.select == 2) console.log("Escolheu o password");
+    });
+
+    prompt.start();
+}
+
+main()
